b65ff15033835d165a68eaea43aac709aad39321
Pxtl-clod <280797458+Pxtl-clod@users.noreply.github.com>
2026-06-07 02:10:59 +0000

    Fix ToIDictionary extension methods
    For extension methods, the 'this' collection parameter MUST be first.
    The factory delegate is correctly the second parameter.
    
    All overloads follow this pattern:
      ToIDictionary(this IEnumerable<T> source, Func<TDictionary> factory)

--- CollectionsSpecializedExtensions/IDictionaryExtensions.cs
+++ CollectionsSpecializedExtensions/IDictionaryExtensions.cs
@@ -7,51 +7,55 @@ public static class IDictionaryExtensions
 {
     /// <summary>
     /// Creates a new <see cref="IDictionary{TKey, TValue}"/> from an <see cref="IEnumerable{T}"/> 
-    /// of <see cref="KeyValuePair{TKey, TValue}"/> source using the dictionaryFactoryFunction to 
-    /// construct it.
+    /// of <see cref="KeyValuePair{TKey, TValue}"/> source using the factory to construct it.
     /// </summary>
-    /// <param name="source">The enumerable source.</param>
-    /// <param name="dictionaryFactoryFunction">The equality comparer.</param>
+    /// <param name="source">The enumerable source of key-value pairs.</param>
+    /// <param name="factory">The factory to create the dictionary.</param>
     /// <typeparam name="TDictionary">Concrete type of the IDictionary.</typeparam>
     /// <typeparam name="TKey">Type of keys of the IDictionary.</typeparam>
     /// <typeparam name="TValue">Type of values in the IDictionary.</typeparam>
     /// <returns>A new <see cref="IDictionary{TKey, TValue}"/> with the mapped data.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="dictionaryFactoryFunction"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="factory"/> is null.</exception>
     public static TDictionary ToIDictionary<TDictionary, TKey, TValue>(
         this IEnumerable<KeyValuePair<TKey, TValue>> source,
-        Func<TDictionary> dictionaryFactoryFunction)
+        Func<TDictionary> factory)
         where TKey : notnull
         where TDictionary: IDictionary<TKey, TValue>
     {
         ArgumentNullException.ThrowIfNull(source, nameof(source));
-        ArgumentNullException.ThrowIfNull(dictionaryFactoryFunction, nameof(dictionaryFactoryFunction));
-        return dictionaryFactoryFunction();
+        ArgumentNullException.ThrowIfNull(factory, nameof(factory));
+        var dictionary = factory();
+        foreach (var item in source)
+        {
+            dictionary[item.Key] = item.Value;
+        }
+        return dictionary;
     }
 
     /// <summary>
     /// Creates a new <see cref="IDictionary{TKey, TSource}"/> from an <see cref="IEnumerable{TSource}"/>
-    /// using the dictionaryFactoryFunction to construct it.
+    /// using the factory to construct it.
     /// </summary>
-    /// <param name="source">The enumerable source.</param>
+    /// <param name="source">The enumerable source of elements.</param>
+    /// <param name="factory">The factory to create the dictionary.</param>
     /// <param name="keySelector">A selector for keys.</param>
-    /// <param name="dictionaryFactoryFunction">The equality comparer.</param>
     /// <typeparam name="TDictionary">Concrete type of the IDictionary.</typeparam>
     /// <typeparam name="TSource">Type of elements in the source.</typeparam>
     /// <typeparam name="TKey">Type of keys of the IDictionary.</typeparam>
     /// <returns>A new <see cref="IDictionary{TKey, TSource}"/> with the mapped data.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="keySelector"/>, or <paramref name="dictionaryFactoryFunction"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="keySelector"/>, or <paramref name="factory"/> is null.</exception>
     public static TDictionary ToIDictionary<TDictionary, TKey, TSource>(
         this IEnumerable<TSource> source,
-        Func<TSource, TKey> keySelector,
-        Func<TDictionary> dictionaryFactoryFunction)
+        Func<TDictionary> factory,
+        Func<TSource, TKey> keySelector)
         where TKey : notnull 
         where TDictionary: IDictionary<TKey, TSource>
     {
         ArgumentNullException.ThrowIfNull(source, nameof(source));
+        ArgumentNullException.ThrowIfNull(factory, nameof(factory));
         ArgumentNullException.ThrowIfNull(keySelector, nameof(keySelector));
-        ArgumentNullException.ThrowIfNull(dictionaryFactoryFunction, nameof(dictionaryFactoryFunction));
 
-        var dictionary = dictionaryFactoryFunction();
+        var dictionary = factory();
         foreach (var element in source)
         {
             dictionary[keySelector(element)] = element;
@@ -60,33 +64,33 @@ public static class IDictionaryExtensions
     }
 
     /// <summary>
-    /// Creates a new <see cref="IDictionary{TKey, TValue}"/> with key and value selectors.
-    /// using the dictionaryFactoryFunction to construct it.
+    /// Creates a new <see cref="IDictionary{TKey, TValue}"/> with key and value selectors 
+    /// using the factory to construct it.
     /// </summary>
-    /// <param name="source">The enumerable source.</param>
+    /// <param name="source">The enumerable source of elements.</param>
+    /// <param name="factory">The factory to create the dictionary.</param>
     /// <param name="keySelector">A selector for keys.</param>
     /// <param name="valueSelector">A selector for values.</param>
-    /// <param name="dictionaryFactoryFunction">The equality comparer.</param>
     /// <typeparam name="TDictionary">Concrete type of the IDictionary.</typeparam>
     /// <typeparam name="TSource">Type of elements in the source.</typeparam>
     /// <typeparam name="TKey">Type of keys of the IDictionary.</typeparam>
-    /// <typeparam name="TValue">Type of values in the IDictionary.</typeparam>
+    /// <typeparam name="TValue">Type of values of the IDictionary.</typeparam>
     /// <returns>A new <see cref="IDictionary{TKey, TValue}"/> with the mapped data.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="keySelector"/>, <paramref name="valueSelector"/>, or <paramref name="dictionaryFactoryFunction"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/>, <paramref name="keySelector"/>, <paramref name="valueSelector"/>, or <paramref name="factory"/> is null.</exception>
     public static TDictionary ToIDictionary<TDictionary, TSource, TKey, TValue>(
         this IEnumerable<TSource> source,
+        Func<TDictionary> factory,
         Func<TSource, TKey> keySelector,
-        Func<TSource, TValue> valueSelector,
-        Func<TDictionary> dictionaryFactoryFunction)
+        Func<TSource, TValue> valueSelector)
         where TKey : notnull
         where TDictionary: IDictionary<TKey, TValue>
     {
         ArgumentNullException.ThrowIfNull(source, nameof(source));
+        ArgumentNullException.ThrowIfNull(factory, nameof(factory));
         ArgumentNullException.ThrowIfNull(keySelector, nameof(keySelector));
         ArgumentNullException.ThrowIfNull(valueSelector, nameof(valueSelector));
-        ArgumentNullException.ThrowIfNull(dictionaryFactoryFunction, nameof(dictionaryFactoryFunction));
 
-        var dictionary = dictionaryFactoryFunction();
+        var dictionary = factory();
         foreach (var element in source)
         {
             var key = keySelector(element);
@@ -98,25 +102,25 @@ public static class IDictionaryExtensions
 
     /// <summary>
     /// Creates a new <see cref="IDictionary{TKey, TValue}"/> from an <see cref="IEnumerable{T}"/> of <see cref="ValueTuple{TKey, TValue}"/> source
-    /// using the dictionaryFactoryFunction to construct it.
+    /// using the factory to construct it.
     /// </summary>
-    /// <param name="source">The enumerable source.</param>
-    /// <param name="dictionaryFactoryFunction">The equality comparer.</param>
+    /// <param name="source">The enumerable source of tups.</param>
+    /// <param name="factory">The factory to create the dictionary.</param>
     /// <typeparam name="TDictionary">Concrete type of the IDictionary.</typeparam>
     /// <typeparam name="TKey">Type of keys of the IDictionary.</typeparam>
     /// <typeparam name="TValue">Type of values in the IDictionary.</typeparam>
     /// <returns>A new <see cref="IDictionary{TKey, TValue}"/> with the mapped data.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="dictionaryFactoryFunction"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="factory"/> is null.</exception>
     public static TDictionary ToIDictionary<TDictionary, TKey, TValue>(
         this IEnumerable<(TKey, TValue)> source,
-        Func<TDictionary> dictionaryFactoryFunction)
+        Func<TDictionary> factory)
         where TKey : notnull
         where TDictionary: IDictionary<TKey, TValue>
     {
         ArgumentNullException.ThrowIfNull(source, nameof(source));
-        ArgumentNullException.ThrowIfNull(dictionaryFactoryFunction, nameof(dictionaryFactoryFunction));
+        ArgumentNullException.ThrowIfNull(factory, nameof(factory));
 
-        var dictionary = dictionaryFactoryFunction();
+        var dictionary = factory();
         foreach (var item in source)
         {
             dictionary[item.Item1] = item.Item2;
